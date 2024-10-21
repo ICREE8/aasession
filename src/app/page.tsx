01c272b96a5d9b9a47adad0388717e6bf966866e
Marduk <arenash8@gmail.com>
2024-10-21 15:25:34 -0400

    add activeAccount

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,20 @@ import Image from "next/image";
 import { ConnectButton } from "thirdweb/react";
 import { client } from "./client";
 import { baseSepolia } from "thirdweb/chains"
+import { getContract } from "thirdweb";
+import { useActiveAccount } from "thirdweb/react";
+
 
 export default function Home() {
+  const smartWallet = useActiveAccount();
+
+  const contract = getContract({
+    client: client,
+    chain: baseSepolia,
+    address: smartWallet?.address as string,
+
+  });
+
   return (
     <div className="flex justify-center items-center h-screen">
       <ConnectButton
