3302863c9a6cf58dbc5920a453f9f3b3201f0a63
Marduk <arenash8@gmail.com>
2024-10-21 00:39:33 -0400

    Account Abstraction and chainSelector

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,6 +1,8 @@
 // src/client.ts
 import { createThirdwebClient } from "thirdweb";
 
+const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
+
 export const client = createThirdwebClient({
-    clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
+    clientId: clientId as string,
 });
